40a98625aaeab2ed2379d042dd7b57147c94467a
claire <claire.mariko.smith@gmail.com>
2016-06-16 20:47:17 +0100

    update

--- client/src/Game/Game.js
+++ client/src/Game/Game.js
@@ -1,11 +1,21 @@
 var Game = function(){
   this.characters = [];
+  this.guest = null;
 }
 
 Game.prototype={
   addCharacter: function(character){
     this.characters.push(character);
   },
+  addGuest:function(character){
+    this.guest = this.characters[Math.floor(Math.random()*this.characters.length)];
+  },
+  checkHair:function(hair){
+    if(!hair) return 'False'
+  }
+  checkGuest:function(character){
+    if(!character) return 'False'
+  }
 
 }
 
